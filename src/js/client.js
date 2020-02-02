33c1f086e9d1aedc0ebb6234a057bc44e9f72c20
Luis Pedro Cuéllar <lpcuellar14@gmail.com>
2020-02-02 17:27:14 -0600

    update

--- src/js/client.js
+++ src/js/client.js
@@ -1,59 +1,100 @@
-
-const renderLuz = ({
-    color,
-    size = 200,
-    isTurnedOn = false,
-}) => {
-    const luz = document.createElement('div');
-    luz.style.width = `${size}px`;
-    luz.style.height = `${size}px`;
-    luz.style.borderRadius = `${size / 2}px`;
-    luz.style.backgroundColor = color;
-    luz.style.opacity = isTurnedOn ? 1.0 : 0.25;
-    return luz;
-}
-
-
-const render = (mount, state) => {
-    const { turnedOnIndex } = state;
-
-    const semaforo = document.createElement('div');
-    semaforo.style.backgroundColor = 'black';
-    semaforo.style.width = '200px';
-    semaforo.style.padding = '25px';
-    [
-        'red',
-        'yellow',
-        'green'
-    ].map(
-        (color, index) => renderLuz({
-            color,
-            isTurnedOn: index === turnedOnIndex,
-        }),
-    ).forEach(
-        luz => semaforo.appendChild(luz),
-    );
-
-    const boton = document.createElement('button');
-    boton.style.width = '250px';
-    boton.style.fontSize = '20px';
-    boton.innerText = 'Siguiente';
-
-    boton.onclick = () => {
-        state.turnedOnIndex = (state.turnedOnIndex + 1) % 3;
-        root.innerHTML = '';
-        render(root, state);
-    };
-
-    mount.appendChild(semaforo);
-    mount.appendChild(boton);
-};
-
-
-const APP_STATE = {
-    turnedOnIndex: 1,
-};
+import _ from 'lodash';
 
 const root = document.getElementById('root');
 
-render(root, APP_STATE);
+const renderLayout = (state, turn, i, j) => {
+    const color = state;
+    const board = document.createElement('div');
+    board.style.width = '60px';
+    board.style.height = '60px';
+    //board.style.borderRadius = '5px';
+    board.style.backgroundColor = 'green';
+    board.style.borderColor = 'darkgreen';
+    board.style.borderWidth = '2px';
+    board.style.borderStyle = 'solid';
+    board.style.alignItems = 'center';
+
+    const ficha = state === 0 ? document.createElement('button') : document.createElement('div');
+    ficha.style.height = '50px';
+    ficha.style.width = '50px';
+    ficha.style.borderRadius = '50%';
+    ficha.style.borderStyle = 'solid';
+    ficha.style.borderWidth = '2px';
+    ficha.style.marginTop = '4px';
+    ficha.style.marginLeft = '4px';
+    //console.log(state);
+
+    if (color != 0) {
+        ficha.style.backgroundColor = color === 1 ? 'black' : 'white';
+        //console.log(state);
+    } else {
+
+        //  generar posibles movimientos
+
+        ficha.style.backgroundColor = '#6b8085';
+        ficha.style.opacity = '0.25';
+        console.log('si entra bro');
+
+
+        ficha.onclick = () => {
+            ficha.style.opacity = '1';
+            app_State.grid[i][j] = app_State.turn === 1 ? 1 : 2;
+            app_State.turn = turn === 1 ? 2 : 1;
+            root.innerHTML = ' ';
+            render(root, app_State);
+            //console.table(app_State.grid);
+        }
+    }
+    board.appendChild(ficha);
+    return board;
+}
+
+const movimientosPosibles = (turn, grid, i, j) => {
+
+}
+
+const render = (mount, state) => {
+
+    const { turn } = state;
+    const { grid } = state;
+
+    //  Creación del tablero
+    const tablero = document.createElement('div');
+    tablero.style.alignItems = 'center';
+    tablero.style.justifyContent = 'center';
+    tablero.style.backgroundColor = 'black';
+    tablero.style.borderRadius = '5px';
+    tablero.style.display = 'flex';
+    tablero.style.flexDirection = 'wrap';
+    tablero.style.flexWrap = 'wrap';
+    tablero.style.height = '512px';
+    tablero.style.padding = '5px';
+    tablero.style.width = '512px';
+    mount.appendChild(tablero);
+
+    const renderGrid = grid;
+
+    //  Creación de las casillas junto con las fichas
+    renderGrid.map(
+        (row, i) => row.map(
+            (column, j) => renderLayout(renderGrid[i][j], turn, i, j)
+        ).forEach(
+            section => tablero.appendChild(section)
+        ));
+};
+
+const app_State = {
+    grid: [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 1, 0, 0, 0],
+        [0, 0, 0, 1, 2, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    turn: 1,
+};
+
+render(root, app_State);
